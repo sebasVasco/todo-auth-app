8364bf168b3cb4c2c84d92078f3442f7988eb700
sebasVasco <skaterionegro9@gmail.com>
2023-04-09 17:46:33 -0500

    adding ng-bootstrap and angular/fire dependencies

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'todo-auth-app-48c80',
+    appId: '1:593597625626:web:ebc580986b91610b9d28a3',
+    storageBucket: 'todo-auth-app-48c80.appspot.com',
+    apiKey: 'AIzaSyBd7wr6zqUFPCG-fUuWryZIuH2wAmSp3DY',
+    authDomain: 'todo-auth-app-48c80.firebaseapp.com',
+    messagingSenderId: '593597625626',
+    measurementId: 'G-LWH8ZVXGGJ',
+  },
+  production: false,
+};
